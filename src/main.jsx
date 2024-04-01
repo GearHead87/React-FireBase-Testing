f0cfc8d9d2fbf3ee9e0a46e52c9df6aabf88402f
GearHead87 <hosanulislam87@gmail.com>
2024-04-01 15:11:50 +0600

    login register page and firebase auth using contextapi

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,10 @@ import {
 } from "react-router-dom";
 import MainLayout from './components/MainLayout/MainLayout';
 import Home from './components/Home.jsx/Home';
+import AuthProvider from './providers/AuthProvider';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+
 
 const router = createBrowserRouter([
   {
@@ -15,8 +19,16 @@ const router = createBrowserRouter([
     element: <MainLayout></MainLayout>,
     children: [
       {
-        path: "home",
-        element: <Home></Home>
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: 'login',
+        element: <Login></Login>,
+      },
+      {
+        path: 'register',
+        element: <Register></Register>,
       }
     ]
   },
@@ -24,6 +36,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>,
 )
